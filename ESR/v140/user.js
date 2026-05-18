37940816b845482c20a808726c42188d0d6abcda
funk-on-code <113871227+funk-on-code@users.noreply.github.com>
2026-05-18 12:41:21 +1000

    Create ESR-based user.js
    My "standard settings" (user.js) for Firefox ESR

---
+++ ESR/v140/user.js
@@ -0,0 +1,10 @@
+//
+/* You may copy+paste this file and use it as it is.
+ *
+ * If you make changes to your about:config while the program is running, the
+ * changes will be overwritten by the user.js when the application restarts.
+ *
+ * To make lasting changes to preferences, you will have to edit the user.js.
+ */
+
+//
