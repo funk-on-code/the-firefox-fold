6efc01aefb694beca82487ab7680dbfdb2b7e0db
funk-on-code <113871227+funk-on-code@users.noreply.github.com>
2026-05-18 14:18:02 +1000

    Updating user.js w/ my commentary.

--- ESR/v140/user.js
+++ ESR/v140/user.js
@@ -7,4 +7,23 @@
  * To make lasting changes to preferences, you will have to edit the user.js.
  */
 
+
+ /* This, yes, is **mostly** from Betterfox: https://github.com/yokoffing/BetterFox */
+
+/* Which, before you start: If you're using "rotating rust, rather than an SSD, change line: XX from X to Y */
+
+/*
+ * ... but, with, it's more optimised for my, security-focused-break-your-ads-work-with-banks-portable-to-mobiles *
+ * ideals, i'll mark (w/ a comment) if you can change it for performance instead, otherwise, these are "good" for *
+ * corporates, or users wanting to see less ads and grief on the internet ;) *
+ */
+
+/* If you want to use my **exact** configuration, you should also install the following plugins:
+
+ -- uBlock Origin
+ -- uMatrix
+ -- Decentraleyes
+
+ As well #hint
+ 
 //
